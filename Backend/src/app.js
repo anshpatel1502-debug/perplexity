ac4102671c48e0a844af39a63398826f3c5b37db
anshpatel1502-debug <anshpatel1502@gmail.com>
2026-05-23 13:37:08 +0530

    new

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(coookieParser());
 app.use(morgan("dev"));
 app.use(cors({
-  origin:process.env.FRONTEND_URL,
+  origin:"http://localhost:5173",
   credentials:true,
   methods:["GET","POST","PUT","DELETE"]
 }))
