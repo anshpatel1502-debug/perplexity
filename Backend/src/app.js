e6e7ae8ac20fde8a9f355f95a482605ff41e46b5
anshpatel1502-debug <anshpatel1502@gmail.com>
2026-06-29 09:57:17 +0530

    fixed0

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(coookieParser());
 app.use(morgan("dev"));
 app.use(cors({
-  //origin:process.env.FRONTEND_URL,
-  origin:"http://localhost:5173",
+  origin:process.env.FRONTEND_URL,
+  // origin:"http://localhost:5173",
   credentials:true,
   methods:["GET","POST","PUT","DELETE"]
 }))
--- Backend/src/sockects/service.socket.js
+++ Backend/src/sockects/service.socket.js
@@ -5,7 +5,7 @@ let io;
 export function initialSocket(httpServer){
   io = new Server(httpServer,{
     cors:{
-      origin:"http://localhost:5173",
+      origin:process.env.FRONTEND_URL,
       credentials:true
     }
   })
